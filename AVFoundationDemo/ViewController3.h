9bb110b42133ebc5f8f66e53fd229dac23976379
Xia_Q <xqstrong@163.com>
2016-07-08 11:48:24 +0800

    no message

---
+++ AVFoundationDemo/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  AVFoundationDemo
+//
+//  Created by Xia_Q on 16/7/8.
+//  Copyright © 2016年 X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIViewController
+
+@end
